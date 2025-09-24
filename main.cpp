32ab1153e92077d4df34dd16eec008fdb2068668
Lauren <owner@Ren.local>
2025-09-24 11:39:11 -0700

    exit if user says nothing (closes #1)

--- main.cpp
+++ main.cpp
@@ -19,11 +19,18 @@ int main(){
   pick = rand() % 4;
   cout << "What are you listening to?\n";
   getline(cin,input);
+  if (input == "nothing") {
+        return 0;
+    }
+
   cout << VALIDATION[pick] << "! Let's listen to more\n";
 
   do{
     cout << "What's next?\n";
     getline(cin,input);
+    if (input == "nothing") {
+        break;
+    }
     pick = rand() % 4;
     cout << VALIDATION[pick] << "!\n";
   }while( input != "nothing" );
