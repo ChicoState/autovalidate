662d85ee77cea7ba6670f95f6fc4eacbdf8d7488
OlekS03 <shestopalovaleks@gmail.com>
2025-09-24 11:24:27 -0700

    Always terminate upon "nothing" fix #1

--- main.cpp
+++ main.cpp
@@ -9,24 +9,35 @@ using std::endl;
 using std::string;
 using std::vector;
 
-const vector <string> VALIDATION = {"Cool","Great","Perfect","Beautiful"};
+const vector<string> VALIDATION = {"Cool", "Great", "Perfect", "Beautiful"};
 
-int main(){
-  string input;
-  int pick;
+int main() {
+    string input;
+    int pick;
 
-  srand(time(0));
-  pick = rand() % 4;
-  cout << "What are you listening to?\n";
-  getline(cin,input);
-  cout << VALIDATION[pick] << "! Let's listen to more\n";
+    srand(time(0));
 
-  do{
-    cout << "What's next?\n";
-    getline(cin,input);
+    cout << "What are you listening to?\n";
+    getline(cin, input);
+
+    if (input == "nothing") {
+        return 0; // Exit immediately if user enters "nothing"
+    }
+
     pick = rand() % 4;
-    cout << VALIDATION[pick] << "!\n";
-  }while( input != "nothing" );
+    cout << VALIDATION[pick] << "! Let's listen to more\n";
 
-  return 0;
+    do {
+        cout << "What's next?\n";
+        getline(cin, input);
+
+        if (input == "nothing") {
+            break; // Exit loop if user enters "nothing"
+        }
+
+        pick = rand() % 4;
+        cout << VALIDATION[pick] << "!\n";
+    } while (true);
+
+    return 0;
 }
