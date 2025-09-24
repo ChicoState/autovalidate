c71ccdaecfd5993771cc5bd7653a3bf85441e4d6
Zach Boggs <55774849+ZachBoggs@users.noreply.github.com>
2025-09-24 11:17:41 -0700

    fix(terminate-nothing) : termates when input is nothing
    fixed the program so now it terminates when the user inputs nothing. I also adjusted some of the syntax.

--- main.cpp
+++ main.cpp
@@ -11,7 +11,8 @@ using std::vector;
 
 const vector <string> VALIDATION = {"Cool","Great","Perfect","Beautiful"};
 
-int main(){
+int main()
+{
   string input;
   int pick;
 
@@ -19,11 +20,22 @@ int main(){
   pick = rand() % 4;
   cout << "What are you listening to?\n";
   getline(cin,input);
+
+  // exiting if the user is listening to nothing
+  // they must be so bored listening to nothing :/
+  if(input == "nothing") { return 0; }
+
   cout << VALIDATION[pick] << "! Let's listen to more\n";
 
-  do{
+  do
+  {
     cout << "What's next?\n";
     getline(cin,input);
+
+    // exiting if the user is listening to nothing
+    // they must be so bored listening to nothing :/
+    if(input == "nothing") { return 0; }
+
     pick = rand() % 4;
     cout << VALIDATION[pick] << "!\n";
   }while( input != "nothing" );
