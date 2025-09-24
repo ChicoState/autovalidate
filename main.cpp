dbb79dd83cd6d89e32e3542d1fccd6c3cec03d3b
Arnav <arnavjain2016@gmail.com>
2025-09-24 11:42:50 -0700

    Resolved Merge conflicts

--- main.cpp
+++ main.cpp
@@ -16,19 +16,19 @@ int main() {
   int pick;
 
   srand(time(0));
-  
+
   cout << "What are you listening to?\n";
   getline(cin, input);
-
   if (input != "nothing") {
-    pick = rand() % 4;
+    pick = rand() % VALIDATION.size();
     cout << VALIDATION[pick] << "! Let's listen to more\n";
 
     do {
       cout << "What's next?\n";
       getline(cin, input);
+      transform(input.begin(), input.end(), input.begin(), [](unsigned char c){ return std::tolower(c); });
       if (input != "nothing") {
-        pick = rand() % 4;
+        pick = rand() % VALIDATION.size();
         cout << VALIDATION[pick] << "!\n";
       }
     } while (input != "nothing");
