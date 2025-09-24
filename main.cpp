30cc69c12338cc3d0b043da2dff838a4ae8e402f
Joshua Gonzalez <jgonzalez98@csuchico.edu>
2025-09-24 11:30:48 -0700

    Setting up continous integration Closes#2

--- main.cpp
+++ main.cpp
@@ -2,35 +2,37 @@
 #include <cstdlib>
 #include <ctime>
 #include <vector>
-#include <cctype>
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
 using std::vector;
-using std::transform;
 
-const vector <string> VALIDATION = {"Cool","Great","Perfect","Beautiful","Aw, yeah"};
+const vector <string> VALIDATION = {"Cool","Great","Perfect","Beautiful"};
 
 int main(){
   string input;
   int pick;
 
   srand(time(0));
-  pick = rand() % VALIDATION.size();
+  pick = rand() % 4;
   cout << "What are you listening to?\n";
   getline(cin,input);
-  transform(input.begin(), input.end(), input.begin(), [](unsigned char c){ return std::tolower(c); });
   cout << VALIDATION[pick] << "! Let's listen to more\n";
 
   do{
     cout << "What's next?\n";
     getline(cin,input);
-    transform(input.begin(), input.end(), input.begin(), [](unsigned char c){ return std::tolower(c); });
-    pick = rand() % VALIDATION.size();
+    pick = rand() % 4;
     cout << VALIDATION[pick] << "!\n";
   }while( input != "nothing" );
+
+        cout << "What's next?\n";
+        getline(cin, input);
+  if(input == nothing){
+        exit(0);
+  }//if
 
   return 0;
 }
