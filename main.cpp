5f6adec95ca98c7bd5300b1109abeb9ab65e296f
Anthony <HDGamerhype@gmail.com>
2026-02-18 11:46:56 -0800

    Added github workflow and fixed main.cpp bug

--- main.cpp
+++ main.cpp
@@ -3,7 +3,10 @@
 #include <ctime>
 #include <vector>
 #include <cctype>
+#include <algorithm>
+#include <cstdlib>
 
+using namespace std;
 using std::cout;
 using std::cin;
 using std::endl;
@@ -13,8 +16,6 @@ using std::transform;
 
 const vector <string> VALIDATION = {"Cool","Great","Perfect","Beautiful","Aw, yeah"};
 
-string get_input_in_lowercase();
-
 int main(){
   string input;
   int pick;
@@ -22,22 +23,22 @@ int main(){
   srand(time(0));
   pick = rand() % VALIDATION.size();
   cout << "What are you listening to?\n";
-  input = get_input_in_lowercase();
+  getline(cin,input);
+  transform(input.begin(), input.end(), input.begin(), [](unsigned char c){ return std::tolower(c); });
+  if (input == "nothing"){
+    return 0;
+  } else {
+    cout << "Nothing? That's not very good for you.\n";
+  }
   cout << VALIDATION[pick] << "! Let's listen to more\n";
 
   do{
     cout << "What's next?\n";
-    input = get_input_in_lowercase();
+    getline(cin,input);
+    transform(input.begin(), input.end(), input.begin(), [](unsigned char c){ return std::tolower(c); });
     pick = rand() % VALIDATION.size();
     cout << VALIDATION[pick] << "!\n";
   }while( input != "nothing" );
 
   return 0;
-}
-
-string get_input_in_lowercase(){
-  string in;
-  getline(cin,in);
-  transform(in.begin(), in.end(), in.begin(), [](unsigned char c){ return std::tolower(c); });
-  return in;
 }
