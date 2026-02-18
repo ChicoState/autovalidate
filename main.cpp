f4b6731b20d6cb54992088f84a8dc03e271170ee
Austin Duckworth <awduckworth@csuchico.edu>
2026-02-18 11:16:27 -0800

    "nothing" quits on first input, resolves Issue #96

--- main.cpp
+++ main.cpp
@@ -21,9 +21,10 @@ int main(){
   pick = rand() % VALIDATION.size();
   cout << "What are you listening to?\n";
   getline(cin,input);
-  transform(input.begin(), input.end(), input.begin(), [](unsigned char c){ return std::tolower(c); });
-  cout << VALIDATION[pick] << "! Let's listen to more\n";
-
+  if (input != "nothing") {
+    transform(input.begin(), input.end(), input.begin(), [](unsigned char c){ return std::tolower(c); });
+    cout << VALIDATION[pick] << "! Let's listen to more\n";
+  }
   do{
     cout << "What's next?\n";
     getline(cin,input);
