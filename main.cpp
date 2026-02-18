abca55a3e16993a59e77085e866dbeda0c68fb61
IJNJJJCG <hardlifecansuck@gmail.com>
2026-02-18 11:48:56 -0800

    Fixed the nothing bug for the nothing input

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <ctime>
 #include <vector>
 #include <cctype>
+#include <algorithm>
 
 using std::cout;
 using std::cin;
@@ -23,14 +24,21 @@ int main(){
   getline(cin,input);
   transform(input.begin(), input.end(), input.begin(), [](unsigned char c){ return std::tolower(c); });
   cout << VALIDATION[pick] << "! Let's listen to more\n";
+  if ( input = "nothing")
+  {
+    cout << VALIDATION[pick] << ". See you later!\n";
+  }
 
   do{
     cout << "What's next?\n";
-    getline(cin,input);
-    transform(input.begin(), input.end(), input.begin(), [](unsigned char c){ return std::tolower(c); });
     pick = rand() % VALIDATION.size();
     cout << VALIDATION[pick] << "!\n";
   }while( input != "nothing" );
+
+  if ( input = "nothing")
+  {
+    cout << VALIDATION[pick] << ". See you later!\n";
+  }
 
   return 0;
 }
