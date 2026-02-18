d4d5724b39c8a49c2c601b69d4867b58325ce55b
SynthNibbler <tmotley@csuchico.edu>
2026-02-18 11:35:42 -0800

    fixed #96, exits clean if user trys to exit on first response

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <ctime>
 #include <vector>
 #include <cctype>
+#include <algorithm>
 
 using std::cout;
 using std::cin;
@@ -14,23 +15,28 @@ using std::transform;
 const vector <string> VALIDATION = {"Cool","Great","Perfect","Beautiful","Aw, yeah"};
 
 int main(){
-  string input;
-  int pick;
+    string input;
+    int pick;
 
-  srand(time(0));
-  pick = rand() % VALIDATION.size();
-  cout << "What are you listening to?\n";
-  getline(cin,input);
-  transform(input.begin(), input.end(), input.begin(), [](unsigned char c){ return std::tolower(c); });
-  cout << VALIDATION[pick] << "! Let's listen to more\n";
-
-  do{
-    cout << "What's next?\n";
-    getline(cin,input);
-    transform(input.begin(), input.end(), input.begin(), [](unsigned char c){ return std::tolower(c); });
+    srand(time(0));
     pick = rand() % VALIDATION.size();
-    cout << VALIDATION[pick] << "!\n";
-  }while( input != "nothing" );
+    cout << "What are you listening to?\n";
+    getline(cin,input);
+    if (input == "nothing") {
+        return 0;
+    }
 
-  return 0;
+    transform(input.begin(), input.end(), input.begin(), [](unsigned char c){ return std::tolower(c); });
+    cout << VALIDATION[pick] << "! Let's listen to more\n";
+
+
+    do{
+        cout << "What's next?\n";
+        getline(cin,input);
+        transform(input.begin(), input.end(), input.begin(), [](unsigned char c){ return std::tolower(c); });
+        pick = rand() % VALIDATION.size();
+        cout << VALIDATION[pick] << "!\n";
+    }while( input != "nothing" );
+
+    return 0;
 }
