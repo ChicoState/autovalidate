8954c100e493f050057b9beb953ff8aa168de36c
Ryan Meline <ryan.c.meline@gmail.com>
2026-02-18 11:31:45 -0800

    fixed merge conflicts, resolves #96

--- main.cpp
+++ main.cpp
@@ -14,6 +14,8 @@ using std::transform;
 
 const vector <string> VALIDATION = {"Cool","Great","Perfect","Beautiful","Aw, yeah"};
 
+string get_input_in_lowercase();
+
 int main(){
   string input;
   int pick;
@@ -21,18 +23,24 @@ int main(){
   srand(time(0));
   pick = rand() % VALIDATION.size();
   cout << "What are you listening to?\n";
-  getline(cin,input);
-  transform(input.begin(), input.end(), input.begin(), [](unsigned char c){ return std::tolower(c); });
+  input = get_input_in_lowercase();
   if(input != "nothing")
     cout << VALIDATION[pick] << "! Let's listen to more\n";
 
-  while( input != "nothing") {
+
+  while( input != "nothing" ) {
     cout << "What's next?\n";
-    getline(cin,input);
-    transform(input.begin(), input.end(), input.begin(), [](unsigned char c){ return std::tolower(c); });
+    input = get_input_in_lowercase();
     pick = rand() % VALIDATION.size();
     cout << VALIDATION[pick] << "!\n";
   };
 
   return 0;
+}
+
+string get_input_in_lowercase(){
+  string in;
+  getline(cin,in);
+  transform(in.begin(), in.end(), in.begin(), [](unsigned char c){ return std::tolower(c); });
+  return in;
 }
